4c89ddea36c7afe1475c135096aff8c417beaa0e
bigwanggang <bigwanggang@163.com>
2017-01-23 19:27:17 +0800

    Update CompareArrayListAndLinkedList.java

--- CompareArrayListAndLinkedList.java
+++ CompareArrayListAndLinkedList.java
@@ -2,8 +2,8 @@
  对比ArrayList和LinkedList的性能
 */
 public class CompareArrayListAndLinkedList {
-
-  public static void main(String[] args) {
+  public stativ void main(String[] args) {
+        //测试ArrayList
         List<Integer> list = new ArrayList<>();
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < 50000; i++) {
@@ -13,7 +13,7 @@ public class CompareArrayListAndLinkedList {
             list.get(i);
         }
 
-        System.out.println("LinkedList: " + String.valueOf(System.currentTimeMillis() - startTime));
+        System.out.println("ArrayList: " + String.valueOf(System.currentTimeMillis() - startTime));
 
         List<Integer> list1 = new LinkedList<>();
         long startTime1 = System.currentTimeMillis();
@@ -24,5 +24,21 @@ public class CompareArrayListAndLinkedList {
             list1.get(i);
         }
         System.out.println("LinkdedList: " + String.valueOf(System.currentTimeMillis() - startTime1));
+
+        List<Integer> list2 = new ArrayList<>();
+        long startTime2 = System.currentTimeMillis();
+        for (int i = 0; i < 500000; i++) {
+            list2.add(0,i);
+        }
+
+        System.out.println("ArrayList: " + String.valueOf(System.currentTimeMillis() - startTime2));
+
+        List<Integer> list3 = new LinkedList<>();
+        long startTime3 = System.currentTimeMillis();
+        for (int i = 0; i < 500000; i++) {
+            list3.add(0, i);
+        }
+
+        System.out.println("LinkdedList: " + String.valueOf(System.currentTimeMillis() - startTime3));
     }
 }
