fd3dfad4a1bca7c3d9c3947b6925444472e4a792
bigwanggang <bigwanggang@163.com>
2017-01-26 17:08:34 +0800

    Create Displayable.java

---
+++ graph/Displayable.java
@@ -0,0 +1,11 @@
+package DataStructureAndAlgorithm.graph;
+
+/**
+ * Created by 10183960 on 2017/1/26.
+ */
+public interface Displayable {
+    public int getX();
+    public int getY();
+
+    public String getName();
+}
